be5356e1c65ce798f0c0478bee0ec78021a9e446
Hi9841 <maor.azarov@gmail.com>
2026-03-06 21:30:53 +0300

    master userdata script

--- Terraform/scripts/userdata_master_join.sh
+++ Terraform/scripts/userdata_master_join.sh
@@ -6,23 +6,45 @@ AwsRegion="us-east-1"
 SsmJoinCpParam="/k8s/NachHi/Join/ControlPlane"
 SsmJoinWorkerParam="/k8s/NachHi/Join/Worker"
 
-CertKey=$(kubeadm init phase upload-certs --upload-certs | tail -n 1)
-WorkerJoinCommand=$(kubeadm token create --print-join-command)
-ControlPlaneJoinCommand="${WorkerJoinCommand} --control-plane --certificate-key ${CertKey}"
+TimestampStr=$(aws ssm describe-parameters \
+    --region "${AwsRegion}" \
+    --parameter-filters "Key=Name,Values=${SsmJoinCpParam}" \
+    --query "Parameters[0].LastModifiedDate" \
+    --output text)
 
-aws ssm put-parameter \
-  --name "${SsmJoinWorkerParam}" \
-  --type "SecureString" \
-  --overwrite \
-  --value "${WorkerJoinCommand}" \
-  --region "${AwsRegion}"
+if [ "$TimestampStr" != "None" ] && [ -n "$TimestampStr" ]; then
+    LastModified=$(date -d "$TimestampStr" +%s)
+    CurrentTime=$(date +%s)
+    Age=$((CurrentTime - LastModified))
+else
+    # If the parameter doesn't exist we set age to a high number to force the first update
+    Age=9999
+fi
 
-aws ssm put-parameter \
-  --name "${SsmJoinCpParam}" \
-  --type "SecureString" \
-  --overwrite \
-  --value "${ControlPlaneJoinCommand}" \
-  --region "${AwsRegion}"
+# Only run your original logic if the parameter is older than 120 seconds
+if [ "$Age" -gt 120 ]; then
+    echo "Parameter is $Age seconds old. Updating now..."
+
+    CertKey=$(kubeadm init phase upload-certs --upload-certs | tail -n 1)
+    WorkerJoinCommand=$(kubeadm token create --print-join-command)
+    ControlPlaneJoinCommand="${WorkerJoinCommand} --control-plane --certificate-key ${CertKey}"
+
+    aws ssm put-parameter \
+      --name "${SsmJoinWorkerParam}" \
+      --type "SecureString" \
+      --overwrite \
+      --value "${WorkerJoinCommand}" \
+      --region "${AwsRegion}"
+
+    aws ssm put-parameter \
+      --name "${SsmJoinCpParam}" \
+      --type "SecureString" \
+      --overwrite \
+      --value "${ControlPlaneJoinCommand}" \
+      --region "${AwsRegion}"
+else
+    echo "Parameter is only $Age seconds old. Skipping update."
+fi
 
 EOF
 
