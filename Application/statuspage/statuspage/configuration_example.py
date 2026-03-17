9fb8304bcc75959c9cd0e5b4b77ddb28aa05ca69
Hi9841 <maor.azarov@gmail.com>
2026-03-17 14:06:55 -0700

    :asd

--- Application/statuspage/statuspage/configuration_example.py
+++ Application/statuspage/statuspage/configuration_example.py
@@ -24,7 +24,7 @@ DATABASE = {
 # Redis database settings. Redis is used for caching and for queuing background tasks. A separate configuration exists
 # for each. Full connection details are required.
 REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
-REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
+REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
 REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', '')
 
 REDIS = {
